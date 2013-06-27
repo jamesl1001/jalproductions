c4d3a713abe4f54d666d4c0fb8872179f7e3d236
jamesl1001 <jamesl1001@msn.com>
2013-06-27 22:53:41 +0100

    Remove duplicate columning classes. Add columns to Programming page

--- about.php
+++ about.php
@@ -10,36 +10,38 @@
         <div id="page" itemscope itemtype="http://data-vocabulary.org/Person">
             <h2>About</h2>
             <div class="centre">
-                <div class="content_box" id="face">
+                <div class="col col-p" id="face">
                     <img src="img/james.jpg" class="circle" alt="James Alexander Lee" itemprop="photo"/>
                 </div>
 
-                <div class="content_box" id="me">
-                    <h3>Me</h3>
-                    <p>Hey! My name is <span itemprop="name">James Alexander Lee</span>, I'm 20 years old and I live in <span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address"><span itemprop="region">Kent</span>, <span itemprop="country-name">UK</span></span>.</p>
-                    <p><span itemprop="affiliation">JaL Productions</span> is my portfolio of multimedia work.</p>
-                    <p>I started JaL Productions back in 2005 after being taught Flash in school and being inspired into the realm of digital arts. From then on I have been creating a variety of multimedia content.</p>
-                </div>
+                <div class="col col-s">
+                    <div id="me">
+                        <h3>Me</h3>
+                        <p>Hey! My name is <span itemprop="name">James Alexander Lee</span>, I'm 20 years old and I live in <span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address"><span itemprop="region">Kent</span>, <span itemprop="country-name">UK</span></span>.</p>
+                        <p><span itemprop="affiliation">JaL Productions</span> is my portfolio of multimedia work.</p>
+                        <p>I started JaL Productions back in 2005 after being taught Flash in school and being inspired into the realm of digital arts. From then on I have been creating a variety of multimedia content.</p>
+                    </div>
 
-                <div class="content_box" id="university">
-                    <h3>University</h3>
-                    <p>I started at the University of Kent, Canterbury in 2010, studying BSc (Hons) Multimedia Technology and Design.</p>
-                    <p>I study a wide range of modules including: web design, photography, film, 2D animation, 3D modelling and programming.</p>
-                </div>
+                    <div id="university">
+                        <h3>University</h3>
+                        <p>I started at the University of Kent, Canterbury in 2010, studying BSc (Hons) Multimedia Technology and Design.</p>
+                        <p>I study a wide range of modules including: web design, photography, film, 2D animation, 3D modelling and programming.</p>
+                    </div>
 
-                <div class="content_box" id="bbc">
-                    <h3>BBC</h3>
-                    <p>As part of my university course, I am currently on a year placement at BBC World Service as a Web Developer.</p>
-                    <p>Visit my <a href="http://blog.jalproductions.co.uk/" target="_blank">blog</a> to see what I'm up to.</p>
-                </div>
+                    <div id="bbc">
+                        <h3>BBC</h3>
+                        <p>As part of my university course, I am currently on a year placement at BBC World Service as a Web Developer.</p>
+                        <p>Visit my <a href="http://blog.jalproductions.co.uk/" target="_blank">blog</a> to see what I'm up to.</p>
+                    </div>
 
-                <div class="content_box" id="lastfm">
-                    <!-- <h3>What I'm Listening To</h3> -->
+                    <!-- <div class="col col-p" id="lastfm">
+                        <h3>What I'm Listening To</h3>
+                    </div> -->
                 </div>
 
                 <hr>
 
-                <div class="content_box" id="languages">
+                <div class="col col-p" id="languages">
                     <h3>Languages</h3>
                     <table>
                         <tr>
@@ -84,7 +86,7 @@
                     </table>
                 </div>
 
-                <div class="content_box" id="software">
+                <div class="col col-s" id="software">
                     <h3>Software</h3>
                     <table>
                         <tr>
