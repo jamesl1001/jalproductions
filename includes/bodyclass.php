8261c22569aa460639eac25e3da2d1010beecf1c
jamesl1001 <jamesl1001@msn.com>
2013-07-08 20:19:20 +0100

    Optimise bodyclass.php

--- includes/bodyclass.php
+++ includes/bodyclass.php
@@ -1,12 +1,4 @@
 <?php
-if( $_SERVER["REQUEST_URI"] == "/" ) {
-	echo "index";
-} else if( $_SERVER["REQUEST_URI"] == "/3d.php" || $_SERVER["REQUEST_URI"] == "/3dmodellingmech.php" || $_SERVER["REQUEST_URI"] == "/3dmodellingrace.php" || $_SERVER["REQUEST_URI"] == "/dt1.php" || $_SERVER["REQUEST_URI"] == "/dt2.php" || $_SERVER["REQUEST_URI"] == "/dt3.php" ) {
-	echo "threed";
-} else {
-	$removeslash = substr($_SERVER["REQUEST_URI"], 1);
-	$removevars = explode("?", $removeslash);
-	$removefiletype = explode(".", $removevars[0]);
-	echo $removefiletype[0];
-}
-?>
+$url = parse_url($_SERVER["REQUEST_URI"]);
+$path = explode("/", $url['path']);
+echo (empty($path[count($path)-1]) ? "index" : $path[count($path)-1]);
