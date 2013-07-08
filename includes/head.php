912c36b2ddbcb8f0bca49a44d8392903d841adb3
jamesl1001 <jamesl1001@msn.com>
2013-07-08 20:19:57 +0100

    Add Facebook Open Graph meta tags

--- includes/head.php
+++ includes/head.php
@@ -2,9 +2,17 @@
 <meta name="description" content="JaL Productions (JaLpro) is my Multimedia Technology and Design portfolio. I create 3D modelling, animation, art and design, games, music, photography, programming and web design."/>
 <meta name="author" content="James Alexander Lee"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
+<meta property="og:title" content="JaL Productions"/>
+<meta property="og:url" content="http://jalproductions.co.uk/"/>
+<meta property="og:image" content="http://jalproductions.co.uk/img/logo.jpg"/>
+<meta property="og:site_name" content="JaL Productions"/>
+<meta property="og:description" content="JaL Productions (JaLpro) is my Multimedia Technology and Design portfolio. I create 3D modelling, animation, art and design, games, music, photography, programming and web design."/>
+
 <title>JaL Productions</title>
 <link rel="stylesheet" type="text/css" href="styles.css" />
 <link rel="shortcut icon" href="img/favicon.ico"/>
+
 <!--[if IE]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
