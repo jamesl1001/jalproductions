c25379b0f7e4e229260a41a4313ad953bb5ee0f1
jamesl1001 <jamesl1001@msn.com>
2013-07-27 15:24:48 +0100

    Style links as buttons on homepage. Create _fractions.scss. Update menu to use fractions.

--- index.php
+++ index.php
@@ -12,7 +12,17 @@
             <div class="centre">
                 <span id="jalproductions">JaL Productions</span>
                 <p class="center_italic">Multimedia Technology and Design Portfolio</p>
-                <p class="center_italic"><a href="3d">3D</a> / <a href="artdesign">Art &amp; Design</a> / <a href="flash">Flash</a> / <a href="music">Music</a> / <a href="photography">Photography</a> / <a href="programming">Programming</a> / <a href="web">Web</a></p>
+
+                <div class="button_wrapper">
+                    <a href="3d"><div class="button seventh icon_after icon_after-3d">3D</div></a>
+                    <a href="artdesign"><div class="button seventh icon_after icon_after-artdesign">Art &amp; Design</div></a>
+                    <a href="flash"><div class="button seventh icon_after icon_after-flash">Flash</div></a>
+                    <a href="music"><div class="button seventh icon_after icon_after-music">Music</div></a>
+                    <a href="photography"><div class="button seventh icon_after icon_after-photography">Photography</div></a>
+                    <a href="programming"><div class="button seventh icon_after icon_after-programming">Programming</div></a>
+                    <a href="web"><div class="button seventh icon_after icon_after-web">Web</div></a>
+                </div>
+
                 <div class="feature">
                     <a href="3d">
                         <img src="http://th00.deviantart.net/fs71/PRE/f/2013/086/0/b/0be1fdbdd20bc748c632ef363db32930-d5zh8ne.jpg" alt="Glass Torus with Caustics"/>
