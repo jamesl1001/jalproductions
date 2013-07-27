d318858c61f33cedf9acbbac01aee6bb2515622e
jamesl1001 <jamesl1001@msn.com>
2013-07-27 12:19:09 +0100

    Add more button icons

--- contact.php
+++ contact.php
@@ -13,15 +13,15 @@
                 <h3>Hire Me</h3>
                 <p>Do you like my work? Would you like to work with me?</p>
                 <p>Send me an email and let me know what you'd like done.</p>
-                <a href="&#109;ailto:james&#64;jalproductions&#46;co&#46;uk" class="button">james&#64;jalproductions&#46;co&#46;uk</a>
+                <a href="&#109;ailto:james&#64;jalproductions&#46;co&#46;uk" class="button icon_after icon_after-contact">james&#64;jalproductions&#46;co&#46;uk</a>
 
                 <h3>Social</h3>
                 <p>Other online presences</p>
-                <div id="open_menu" class="button">Social links</div>
+                <a href="#menu" id="open_menu" class="button icon_after icon_after-menu">Social links</a>
 
                 <h3>CV</h3>
                 <p>07/2013</p>
-                <a href="download/JamesLeeCVOnline.docx" class="button">Download my CV</a>
+                <a href="download/JamesLeeCVOnline.docx" class="button icon_after icon_after-download">Download my CV</a>
             </div>
         </div>
 
