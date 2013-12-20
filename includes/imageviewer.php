b08ca74366a4bc628db18a7596b63e7a7577078d
jamesl1001 <jamesl1001@msn.com>
2013-12-20 00:05:01 +0000

    Add left and right arrow markup

--- includes/imageviewer.php
+++ includes/imageviewer.php
@@ -1,4 +1,6 @@
 <div class="iv">
+    <div class="iv-nav iv-left"></div>
+    <div class="iv-nav iv-right"></div>
     <div class="iv-image-wrapper">
         <img src="http://th05.deviantart.net/fs70/PRE/f/2012/013/8/6/863848d2a94808986e7521af2e9bbab5-d4m6md6.jpg"/>
     </div>
