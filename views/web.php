d6e8cb28eca70364f591ca266fdb7b54f2ce174b
jamesl1001 <jamesl1001@msn.com>
2014-07-20 16:38:45 +0100

    Add subsection buttons to Web page

--- views/web.php
+++ views/web.php
@@ -1,11 +1,12 @@
 <h2>Web</h2>
 <div class="centre">
     <?php
-        $site  = (isset($_GET['site']) ? $_GET['site'] : '');
-        $feed  = simplexml_load_file(__DIR__ . '/web.xml');
-        $item  = $feed->xpath("//item[@short='$site']");
-        $newer = $feed->xpath("//item[@short='$site']/preceding-sibling::*[1]");
-        $older = $feed->xpath("//item[@short='$site']/following-sibling::*[1]");
+        $subsection = (isset($_GET['subsection']) ? $_GET['subsection'] : '');
+        $site       = (isset($_GET['site']) ? $_GET['site'] : '');
+        $feed       = simplexml_load_file(__DIR__ . '/web.xml');
+        $item       = $feed->xpath("//item[@short='$site']");
+        $newer      = $feed->xpath("//item[@short='$site']/preceding-sibling::*[1]");
+        $older      = $feed->xpath("//item[@short='$site']/following-sibling::*[1]");
 
         if(!empty($item)) {
             // Single item
@@ -35,9 +36,23 @@
             </div>
         </div>
     <?php
-        } else {
-            // All items
+        } else { ?>
+
+    <div class="col col-p">
+        <a href="web?subsection=bbc"><div class="button web_subsection"><strong>BBC</strong><p>BBC projects during my placement year</p></div></a>
+        <a href="web?subsection=precisepixels"><div class="button web_subsection"><strong>Precise Pixels</strong><p>Client projects by Precise Pixels</p></div></a>
+        <a href="web?subsection=clients"><div class="button web_subsection"><strong>Clients</strong><p>Client projects by JaL Productions</p></div></a>
+    </div>
+
+    <div class="col col-s">
+        <a href="web?subsection=university"><div class="button web_subsection"><strong>University</strong><p>Projects for University modules</p></div></a>
+        <a href="web?subsection=personal"><div class="button web_subsection"><strong>Personal</strong><p>Personal projects done in my spare time</p></div></a>
+        <a href="web?subsection=jal"><div class="button web_subsection"><strong>JaL Productions</strong><p>Revisions of this site</p></div></a>
+    </div>
+
+    <?php  // All items
             foreach($feed->item as $item):
+                if(!empty($subsection) && $item->subsection != $subsection) { continue; };
                 getData($item);
             ?>
             <div class="web_project" id="<?= $short; ?>">
