d4d8adb76f2871226710007ca5b16a952aab2907
jamesl1001 <jamesl1001@msn.com>
2014-07-21 19:11:10 +0100

    Add 404 page

---
+++ views/404.php
@@ -0,0 +1,5 @@
+<h2>404</h2>
+<div class="centre">
+    <h3>Page not found</h3>
+    <p>Sorry, the page you are looking for has not been found. Try checking the URL or <a href="/">return to the homepage</a>.</p>
+</div>
