c4f48feabf3c24e6574cfcbd5729f07520f5bb1f
jamesl1001 <jamesl1001@msn.com>
2014-10-06 11:31:34 +0100

    Update About and Web copy

--- views/about.php
+++ views/about.php
@@ -15,32 +15,17 @@
             <p><span itemprop="affiliation">JaL Productions</span> is my portfolio of multimedia work.</p>
             <p>I started JaL Productions back in 2005 after being taught Flash in school and being inspired into the realm of digital arts. From then on I have been creating a variety of multimedia content.</p>
         </div>
-    </div>
-
-    <div class="col col-s">
-
-        <div id="university">
-            <h3>University</h3>
-            <p>I started at the University of Kent, Canterbury in 2010, studying BSc (Hons) Multimedia Technology and Design.</p>
-            <p>I study a wide range of modules including web design, photography, film, 2D animation, 3D modelling, visual effects, Android development and programming in several languages.</p>
-        </div>
-
-        <div id="bbc">
-            <h3>BBC</h3>
-            <p>Between the second and third year of university, I took an industrial placement year at the BBC World Service as a Web Developer.</p>
-            <p>During my time there I worked on an <a href="http://jalproductions.co.uk/web?site=bbcwsolympics">Olympics special</a> and the <a href="http://jalproductions.co.uk/web?site=bbcwsresponsive">Responsive project</a> to migrate the World Service language sites to responsive design.</p>
-        </div>
 
         <div id="lastfm">
             <h3>What I'm Listening To</h3>
             <?php
-                $url    = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jamesl1001&api_key=547536d9928038ef6de3642606f6557a';
+                $url  = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jamesl1001&api_key=547536d9928038ef6de3642606f6557a';
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
                     CURLOPT_RETURNTRANSFER => 1,
                     CURLOPT_URL => $url,
                     CURLOPT_USERAGENT => 'JaL-User-Agent'));
-                $feed = simplexml_load_string(curl_exec($curl));
+                $feed   = simplexml_load_string(curl_exec($curl));
                 $track  = $feed->recenttracks->track[0];
                 $artist = (string)$track->artist;
                 $name   = (string)$track->name;
@@ -60,6 +45,22 @@
                     </p>
                 </div>
             </a>
+        </div>
+    </div>
+
+    <div class="col col-s">
+        <div id="university">
+            <h3>University</h3>
+            <p>I graduated from the University of Kent, Canterbury in 2014 with a First Class Honours degree in BSc (Hons) Multimedia Technology and Design with a Year in Industry.</p>
+            <p>I studied a wide range of modules including web design, photography, film, 2D animation, 3D modelling, visual effects, Android development and programming in several languages.</p>
+            <p>I am currently still at the University of Kent studying for a PhD in Digital Arts with an emphasis on web technologies and mobile devices.</p>
+        </div>
+
+        <div id="work">
+            <h3>Industry Experience</h3>
+            <p>Between the second and third year of university, I took an industrial placement year at the BBC as a Web Developer for the World Service.</p>
+            <p>During my time there I worked on an <a href="web?site=bbcwsolympics">Olympics special</a> and the <a href="web?site=bbcwsresponsive">Responsive project</a> to migrate the World Service language sites to responsive web design.</p>
+            <p>I also have experience with client-based work, building websites and web apps for restaurants, transport services, charities and start-ups in the fashion and housing industries. For more details about each of these projects, go to <a href="web">Web</a>.</p>
         </div>
     </div>
 
