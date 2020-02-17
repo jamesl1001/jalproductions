5979bf2b7da59f36c552697e6c81098fa2593aad
jamesl1001 <jamesl1001@msn.com>
2020-02-17 21:38:25 +0000

    Add new site banner

--- views/index.php
+++ views/index.php
@@ -15,4 +15,49 @@
     <div class="feature">
         <a href="https://www.youtube.com/watch?v=iQtAZdkXWVU" target="_blank" data-embed="https://www.youtube.com/embed/iQtAZdkXWVU?color=white&amp;modestbranding=1&amp;rel=0" id="youtube_embed"><img src="https://img.youtube.com/vi/iQtAZdkXWVU/maxresdefault.jpg" alt="Snail Caravan &mdash; 3D Animation"/></a>
     </div>
+
+    <div class="new_site_banner"><a href="https://jamesalexanderlee.co.uk"><img src="https://jamesalexanderlee.co.uk/img/old-site-banner.png"/></a></div>
+
+    <style>
+        .new_site_banner {
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0,0,0,.85);
+            position: fixed;
+            top: 0;
+            left: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
+
+        .new_site_banner img {
+            width: 100%;
+            max-width: 1200px;
+            outline: 1px solid #fff;
+        }
+
+        .new_site_banner::before {
+            content: '';
+            width: 40px;
+            height: 40px;
+            background-image: url(img/header_sprites-x2.png);
+            background-repeat: no-repeat;
+            background-position: center -320px;
+            position: fixed;
+            top: 80px;
+            cursor: pointer;
+        }
+    </style>
+
+    <script>
+        var banner = document.querySelector('.new_site_banner');
+
+        banner.addEventListener('click', function(e) {
+            if(e.target.tagName !== 'IMG') {
+                banner.style.display = 'none';
+            }
+        });
+    </script>
 </div>
